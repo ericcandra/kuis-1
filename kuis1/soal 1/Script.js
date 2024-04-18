fcbcbabf1bf3f58edc55c3002304bbf5e60c3ffc
ericcandra <160827840+ericcandra@users.noreply.github.com>
2024-04-18 14:49:24 +0700

    update soal 1

--- kuis1/soal 1/Script.js
+++ kuis1/soal 1/Script.js
@@ -1,19 +1,22 @@
 // lanjutkan script ini ya ...
-let data = ["Arif"];
-let listhobi = ["membaca","bermaingame","belajar"]
+let data = ["membaca","bermain game","coding"];
+let listnama = document.getElementById("listnama")
 
+data.forEach(showWebsiteDebuggingdanMemperbaikiProgram);
+
+function showWebsiteDebuggingdanMemperbaikiProgram(velue, index){
+    console.log(velue);
+    listhobi.innerHTML += "<li>" + velue + "</li>";
+}
 
 let nama = document.getElementById("nama");
 // tampilkan data localstrorage ke dalam elemen <p id="nama">
 nama.innerHTML = localStorage.getItem("nama");
 
-let hobi = document.getElementById("hobi");
+let hobi = ["arif"];
 localStorage.setItem("hobi", hobi);
 localStorage.setItem("myhobi", JSON.stringify (hobi));
+nama.innerHTML = localStorage.getItem("hobi");
 
-let  = {nama: "arif", hobi: "bermaingame"};
-// cara akses
-console.log(mhs.nama);
-console.log(mhs.hobi);
 // ...
 // ...
--- kuis1/soal 2/Script.js
+++ kuis1/soal 2/Script.js
@@ -2,4 +2,5 @@ let npm = 2226240001;
 let nama = "Arif";
 let nilai = [80, 70, 90, 85, 60, 88];
 
+
 // lanjutkan scriptnya ya ...
